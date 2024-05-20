16f6e205ecf38e130e83e66d451e8891542df1e0
Thomas Marquis <thomas.marquis.dev@gmail.com>
2024-05-20 22:45:21 +0200

    setup preatty git log command

--- lua/thom/keymaps/git.lua
+++ lua/thom/keymaps/git.lua
@@ -4,7 +4,12 @@ M.setup_keymaps = function()
 	vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 	vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
 	vim.keymap.set("n", "<leader>gy", ":Gdiffsplit<CR>", {})
-	vim.keymap.set("n", "<leader>gl", ":Git log --oneline<CR>", {})
+	vim.keymap.set(
+		"n",
+		"<leader>gl",
+		":Git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all<CR>",
+		{}
+	)
 end
 
 return M

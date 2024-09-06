652db941b6bf5c8947c58c6e8a12a7b0f200480c
Thomas Marquis <thomas.marquis.dev@gmail.com>
2024-09-06 21:30:52 +0200

    fix helm lsp

--- lua/thom/lsp/settings/helm_ls.lua
+++ lua/thom/lsp/settings/helm_ls.lua
@@ -1,5 +1,13 @@
 local nvim_lsp = require("lspconfig")
 
+vim.filetype.add({
+	pattern = {
+		[".*/templates/.*%.yaml"] = "helm",
+		[".*/templates/.*%.tpl"] = "helm",
+	},
+})
+
 return {
-	root_dir = nvim_lsp.util.root_pattern("Chart.yaml", "Chart.yml"),
+	root_dir = nvim_lsp.util.root_pattern("Chart.yaml", "Chart.yml", "**/Chart.yaml", "**/Chart.yml"),
+	filetypes = { "yaml", "helm" },
 }
--- lua/thom/lsp/settings/yamlls.lua
+++ lua/thom/lsp/settings/yamlls.lua
@@ -6,4 +6,9 @@ return {
 			},
 		},
 	},
+	on_attach = function(client, bufnr)
+		if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
+			vim.diagnostic.enable(false)
+		end
+	end,
 }

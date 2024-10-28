d5809846dc35b48d132f1a15b48e1b4366444cd3
Thomas Marquis <thomas.marquis.dev@gmail.com>
2024-10-28 07:30:50 +0100

    All Done!

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "plugins" },
   },
   defaults = {

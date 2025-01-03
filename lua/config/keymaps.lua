1f8ea4a92e3e9bcf1c994c26a36ab90e13eb67c9
thomas.marquis <thomas.marquis.dev@gmail.com>
2025-01-03 16:56:13 +0100

    update lazy and fix copilot

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,6 @@ local opts = { noremap = true, silent = true }
 --Remap space as leader key
 map("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
 -- NORMAL
 -- Move

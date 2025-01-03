1cdfae2c89dedb41996d4f10bdc860dc47527033
thomas.marquis <thomas.marquis.dev@gmail.com>
2025-01-03 18:56:40 +0100

    fix pyenv venv selector

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -1,6 +1,25 @@
 return {
   {
     "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      "mfussenegger/nvim-dap-python",
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    lazy = false,
+    branch = "regexp",
+    config = function()
+      require("venv-selector").setup({
+        settings = {
+          search = {
+            pyenv = {
+              command = "$FD '/bin/python$' ~/.pyenv/versions --full-path --color never -E pkgs/ -E envs/ -L -u",
+            },
+          },
+        },
+      })
+    end,
     keys = {
       { "<leader>vv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" },
       { "<leader>cv", false },
